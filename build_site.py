5818ae5116a9e66d38208991dcb52d75486c3bb6
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-04-15 17:40:42 +0200

    Fix config usage

--- build_site.py
+++ build_site.py
@@ -25,13 +25,10 @@ from string import Template
 # Import 3rd party libraries
 from tabulate import tabulate
 
-LANG_FIELDS = ["Name", "Glottocode", "Latitude", "Longitude", "Family"]
-PARAM_FIELDS = ["Name", "Concepticon_ID", "Concepticon_Gloss"]
-
 # TODO: what to do with mandatory replacements (using safe_substitute now)
 
 # TODO: properly implement with pycldf, currently reading with csv
-def read_cldf_tables(base_path):
+def read_cldf_data(base_path, config):
     cldf_data = {}
 
     # import for temporary implementation
@@ -42,7 +39,7 @@ def read_cldf_tables(base_path):
     with open(lang_path.as_posix()) as csvfile:
         reader = csv.DictReader(csvfile)
         cldf_data["languages"] = [
-            [row[field] for field in LANG_FIELDS] for row in reader
+            [row[field] for field in config['language_fields']] for row in reader
         ]
 
     # Read paramters table
@@ -50,13 +47,13 @@ def read_cldf_tables(base_path):
     with open(param_path.as_posix()) as csvfile:
         reader = csv.DictReader(csvfile)
         cldf_data["concepts"] = [
-            [row[field] for field in PARAM_FIELDS] for row in reader
+            [row[field] for field in config['parameter_fields']] for row in reader
         ]
 
     return cldf_data
 
 
-def build_index(template, replacements, output_path):
+def build_index(template, replaces, config):
     """
     Build and write `index.html`.
 
@@ -64,20 +61,20 @@ def build_index(template, replacements, output_path):
     ----------
     template : str
         Source for the HTML template.
-    replacements : dict
-        Dictionary of replacements for template.
-    output_path : pathlib.Path
-        Base path of the output directory.
+    replaces : dict
+        A dictionary of replaces for filling the template.
+    config : dict
+        A dictionary with the configurations.
     """
 
     # Apply replacements
     logging.info("Applying replacements to generate `index.html`...")
     source = Template(template)
-    source = source.safe_substitute(replacements)
+    source = source.safe_substitute(replaces)
     logging.info("`index.html` has %i bytes.", len(source))
 
     # write
-    index_path = output_path / "index.html"
+    index_path = config['output_path'] / "index.html"
     write_html(source, index_path.as_posix())
 
 
@@ -87,33 +84,33 @@ def write_html(source, filename):
         handler.write(source)
 
 
-def build_tables(data, replaces, template, output_path):
+def build_tables(data, replaces, template, config):
     # write languages
     html_table = tabulate(
-        data["languages"], headers=LANG_FIELDS, tablefmt="html"
+        data["languages"], headers=config['language_fields'], tablefmt="html"
     )
     lang_replace = replaces.copy()
     lang_replace["home_nosb_main"] = html_table
     lang_source = Template(template)
     lang_source = lang_source.safe_substitute(lang_replace)
 
-    lang_path = output_path / "languages.html"
+    lang_path = config['output_path'] / "languages.html"
     write_html(lang_source, lang_path.as_posix())
 
     # write concepts
     html_table = tabulate(
-        data["concepts"], headers=PARAM_FIELDS, tablefmt="html"
+        data["concepts"], headers=config['parameter_fields'], tablefmt="html"
     )
     param_replace = replaces.copy()
     param_replace["home_nosb_main"] = html_table
     param_source = Template(template)
     param_source = param_source.safe_substitute(param_replace)
 
-    param_path = output_path / "parameters.html"
+    param_path = config['output_path']  / "parameters.html"
     write_html(param_source, param_path.as_posix())
 
 
-def load_templates(base_path):
+def load_templates(config):
     """
     Load and return the sources for the basic HTML templates.
 
@@ -122,8 +119,8 @@ def load_templates(base_path):
 
     Parameters
     ----------
-    base_path : pathlib.Path
-        Base path of the deployment system.
+    config : dict
+        A dictionary with the configurations.
 
     Returns
     -------
@@ -136,13 +133,13 @@ def load_templates(base_path):
     logging.info("Loading the sidebar HTML template...")
 
     # Load with sidebar
-    template_file = base_path / "template" / "with_sidebar.html"
+    template_file = config['base_path'] / "template" / "with_sidebar.html"
     with open(template_file.as_posix()) as handler:
         sidebar_template = handler.read()
     logging.info("Loaded template of %i bytes.", len(sidebar_template))
 
     # Load without sidebar
-    template_file = base_path / "template" / "no_sidebar.html"
+    template_file = config['base_path'] / "template" / "no_sidebar.html"
     with open(template_file.as_posix()) as handler:
         nosidebar_template = handler.read()
     logging.info("Loaded template of %i bytes.", len(nosidebar_template))
@@ -185,7 +182,10 @@ def load_config(base_path):
 
         return source
 
-    # Build replacement dictionary; remember that, in order to make
+    # Build replacement dictionary; which for future expansions it is
+    # preferable to keep separate from the actual configuration while
+    # using a single file not to scare potential users with too much
+    # structure to learn. Remember that, in order to make
     # deployment easy, we are being quite strict here in terms of
     # templates, etc.
     replaces = {
@@ -193,7 +193,7 @@ def load_config(base_path):
         "description" : config.pop("description"),
         "author" : config.pop("author"),
         "icon" : config.pop("icon"), # TODO: rename to favicon
-        "mainlink": config.pop("mainlink"),
+        "mainlink": config.pop("mainlink"), # TODO: should be derived from URL?
         "citation": config.pop("citation"),
         "footer" : _md2html(config.pop("footer_file"), base_path),
         "home_sb_main" : _md2html(config.pop("index_file"), base_path),
@@ -207,24 +207,25 @@ def main():
     Entry point for the script.
     """
 
-    # Obtain `base_path` for file manipulation, as well as the output one
-    # TODO: allow user-specified output
+    # Obtain `base_path` for file manipulation
     base_path = Path(__file__).parent.resolve()
-    output_path = base_path / "_site"
 
-    # Load JSON configuration, hard-coded path
+    # Load JSON configuration and replaces, and include paths in the first
+    # TODO: allow user-specified output
     config, replaces = load_config(base_path)
+    config['base_path'] = base_path
+    config['output_path'] = base_path / "_site"
 
     # Load HTML templates
-    sb_template, nosb_template = load_templates(base_path)
+    sb_template, nosb_template = load_templates(config)
 
     # Build and write index.html
     # TODO: read replace dictionary
-    build_index(sb_template, replaces, output_path)
+    build_index(sb_template, replaces, config)
 
     # Read data tables from CLDF files
-    cldf_data = read_cldf_tables(base_path)
-    build_tables(cldf_data, replaces, nosb_template, output_path)
+    cldf_data = read_cldf_data(base_path, config)
+    build_tables(cldf_data, replaces, nosb_template, config)
 
 
 if __name__ == "__main__":
