937dbddd5c396448b4d53d1ce43b8ef84d6027f5
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-04-15 18:06:31 +0200

    Add recursive replacaments

--- build_site.py
+++ build_site.py
@@ -26,7 +26,38 @@ from string import Template
 import markdown
 from tabulate import tabulate
 
-# TODO: what to do with mandatory replacements (using safe_substitute now)
+
+def fill_template(template, replaces):
+    """
+    Fills a template recursively with the provided replaces.
+
+    Parameters
+    ----------
+    template : str
+        The HTML template to be filled.
+    replaces : dict
+        A dictionary of replacements to be applied recursively.
+
+    Returns
+    -------
+    source : str
+        The HTML source resulting from template replacement.
+    """
+
+    # NOTE: For the time being this is using Python's standard and simple
+    # `Template` object from the `string` library, but we should considering
+    # allowing non-default and more complex methods of replacements and
+    # HTML generation.
+    source = template
+    while True:
+        new_source = Template(source).safe_substitute(replaces)
+        if new_source == source:
+            break
+        else:
+            source = new_source
+
+    return source
+
 
 # TODO: properly implement with pycldf, currently reading with csv
 def read_cldf_data(base_path, config):
@@ -40,7 +71,8 @@ def read_cldf_data(base_path, config):
     with open(lang_path.as_posix()) as csvfile:
         reader = csv.DictReader(csvfile)
         cldf_data["languages"] = [
-            [row[field] for field in config['language_fields']] for row in reader
+            [row[field] for field in config["language_fields"]]
+            for row in reader
         ]
 
     # Read paramters table
@@ -48,7 +80,8 @@ def read_cldf_data(base_path, config):
     with open(param_path.as_posix()) as csvfile:
         reader = csv.DictReader(csvfile)
         cldf_data["concepts"] = [
-            [row[field] for field in config['parameter_fields']] for row in reader
+            [row[field] for field in config["parameter_fields"]]
+            for row in reader
         ]
 
     return cldf_data
@@ -70,12 +103,11 @@ def build_index(template, replaces, config):
 
     # Apply replacements
     logging.info("Applying replacements to generate `index.html`...")
-    source = Template(template)
-    source = source.safe_substitute(replaces)
+    source = fill_template(template, replaces)
     logging.info("`index.html` has %i bytes.", len(source))
 
     # write
-    index_path = config['output_path'] / "index.html"
+    index_path = config["output_path"] / "index.html"
     write_html(source, index_path.as_posix())
 
 
@@ -88,26 +120,24 @@ def write_html(source, filename):
 def build_tables(data, replaces, template, config):
     # write languages
     html_table = tabulate(
-        data["languages"], headers=config['language_fields'], tablefmt="html"
+        data["languages"], headers=config["language_fields"], tablefmt="html"
     )
-    lang_replace = replaces.copy()
-    lang_replace["home_nosb_main"] = html_table
-    lang_source = Template(template)
-    lang_source = lang_source.safe_substitute(lang_replace)
+    lang_replaces = replaces.copy()
+    lang_replaces["home_nosb_main"] = html_table
+    lang_source = fill_template(template, lang_replaces)
 
-    lang_path = config['output_path'] / "languages.html"
+    lang_path = config["output_path"] / "languages.html"
     write_html(lang_source, lang_path.as_posix())
 
     # write concepts
     html_table = tabulate(
-        data["concepts"], headers=config['parameter_fields'], tablefmt="html"
+        data["concepts"], headers=config["parameter_fields"], tablefmt="html"
     )
-    param_replace = replaces.copy()
-    param_replace["home_nosb_main"] = html_table
-    param_source = Template(template)
-    param_source = param_source.safe_substitute(param_replace)
+    param_replaces = replaces.copy()
+    param_replaces["home_nosb_main"] = html_table
+    param_source = fill_template(template, param_replaces)
 
-    param_path = config['output_path']  / "parameters.html"
+    param_path = config["output_path"] / "parameters.html"
     write_html(param_source, param_path.as_posix())
 
 
@@ -134,18 +164,19 @@ def load_templates(config):
     logging.info("Loading the sidebar HTML template...")
 
     # Load with sidebar
-    template_file = config['base_path'] / "template" / "with_sidebar.html"
+    template_file = config["base_path"] / "template" / "with_sidebar.html"
     with open(template_file.as_posix()) as handler:
         sidebar_template = handler.read()
     logging.info("Loaded template of %i bytes.", len(sidebar_template))
 
     # Load without sidebar
-    template_file = config['base_path'] / "template" / "no_sidebar.html"
+    template_file = config["base_path"] / "template" / "no_sidebar.html"
     with open(template_file.as_posix()) as handler:
         nosidebar_template = handler.read()
     logging.info("Loaded template of %i bytes.", len(nosidebar_template))
 
     return sidebar_template, nosidebar_template
+
 
 def load_config(base_path):
     """
@@ -190,18 +221,19 @@ def load_config(base_path):
     # deployment easy, we are being quite strict here in terms of
     # templates, etc.
     replaces = {
-        "title" : config.pop("title"),
-        "description" : config.pop("description"),
-        "author" : config.pop("author"),
-        "icon" : config.pop("icon"), # TODO: rename to favicon
-        "mainlink": config.pop("mainlink"), # TODO: should be derived from URL?
+        "title": config.pop("title"),
+        "description": config.pop("description"),
+        "author": config.pop("author"),
+        "icon": config.pop("icon"),  # TODO: rename to favicon
+        "mainlink": config.pop("mainlink"),  # TODO: should be derived from URL?
         "citation": config.pop("citation"),
-        "footer" : _md2html(config.pop("footer_file"), base_path),
-        "home_sb_main" : _md2html(config.pop("index_file"), base_path),
-        "home_sidebar" : _md2html(config.pop("sidebar_file"), base_path),
+        "footer": _md2html(config.pop("footer_file"), base_path),
+        "home_sb_main": _md2html(config.pop("index_file"), base_path),
+        "home_sidebar": _md2html(config.pop("sidebar_file"), base_path),
     }
 
     return config, replaces
+
 
 def main():
     """
@@ -214,8 +246,8 @@ def main():
     # Load JSON configuration and replaces, and include paths in the first
     # TODO: allow user-specified output
     config, replaces = load_config(base_path)
-    config['base_path'] = base_path
-    config['output_path'] = base_path / "_site"
+    config["base_path"] = base_path
+    config["output_path"] = base_path / "_site"
 
     # Load HTML templates
     sb_template, nosb_template = load_templates(config)
